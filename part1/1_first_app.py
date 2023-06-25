4b4e6d7f90bc1c1ae52320fbc3c69814cba7fdc1
musiakam <kamil.musial@tietoevry.com>
2023-06-25 09:02:24 +0200

    a

--- part1/1_first_app.py
+++ part1/1_first_app.py
@@ -1,3 +1,21 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
+import sys
 
+app = QApplication(sys.argv)
 
+window1 = QDialog()    #proste okno, tylko z Xem do zamkniecia
+window1.setWindowTitle('QDialog')
+
+window2 = QWidget()    #proste okno, z Xem, minimalizacją i pełnym ekranem
+window2.setWindowTitle('QWidget')
+
+window3 = QMainWindow()    #zaawane okno z większą liczbą funkcji
+window3.setWindowTitle('QMainWindow')
+window3.statusBar().showMessage('Status to......')
+window3.menuBar().addMenu('Options')
+
+window1.show()
+window2.show()
+window3.show()
+
+app.exec()
